47f46cc2f2808e7dd803e7dcc7e8f3a805d4c3df
Soumay-sanpui <somusanpui9@gmail.com>
2025-04-26 05:45:52 +0000

    + Controller Bifercation

--- routes/auth.js
+++ routes/auth.js
@@ -1,59 +1,13 @@
 import express from 'express';
 import {
-  register,
-  login,
-  forgotPassword,
-  verifyOtp,
-  resetPassword,
-  getAllUsers,
-  updateName,
-  updateGender,
-  updatePronouns,
-  updateNationality,
-  updateResidentCountry,
-  updateKnownLanguages,
-  updateTimeZone,
-  updateQualification,
-  updateAchievements,
-  updateSkills,
-  updateResume,
-  updateCoverLetter,
-  updateDreamJob,
-  updatePreferredJobTypes,
-  updateWorkEnvPreferences,
-  updatePreferredLocations,
-  updateVideoIntro,
-  updateMobilityPreferences,
-  updateInterviewMode,
-  updateBrandingStatement,
-  updateHobbies,
-  updateEmergencyContact,
-  updateSocialLinks,
-  // Import GET controllers
-  getUserProfile,
-  getName,
-  getGender,
-  getPronouns,
-  getNationality,
-  getResidentCountry,
-  getKnownLanguages,
-  getTimeZone,
-  getQualification,
-  getAchievements,
-  getSkills,
-  getResume,
-  getCoverLetter,
-  getDreamJob,
-  getPreferredJobTypes,
-  getWorkEnvPreferences,
-  getPreferredLocations,
-  getVideoIntro,
-  getMobilityPreferences,
-  getInterviewMode,
-  getBrandingStatement,
-  getHobbies,
-  getEmergencyContact,
-  getSocialLinks
+    forgotPassword,
+    getAllUsers,
+    login,
+    register,
+    resetPassword,
+    verifyOtp,
+    updateUserProfile,
+    getUser
 } from '../controllers/authController.js';
 import {protect} from "../middleware/auth.js";
 
@@ -67,57 +21,11 @@ router.post('/verifyotp', verifyOtp);
 router.post('/resetpassword', resetPassword);
 
 // User routes
-router.get('/users', getAllUsers);
-router.get('/profile', protect, getUserProfile);
+router.get('/users', protect, getAllUsers);
+router.get('/user:id', getUser);
 
-// Individual field update routes
-router.put('/update/name', protect, updateName);
-router.put('/update/gender', protect, updateGender);
-router.put('/update/pronouns', protect, updatePronouns);
-router.put('/update/nationality', protect, updateNationality);
-router.put('/update/resident-country', protect, updateResidentCountry);
-router.put('/update/languages', protect, updateKnownLanguages);
-router.put('/update/timezone', protect, updateTimeZone);
-router.put('/update/qualification', protect, updateQualification);
-router.put('/update/achievements', protect, updateAchievements);
-router.put('/update/skills', protect, updateSkills);
-router.put('/update/resume', protect, updateResume);
-router.put('/update/cover-letter', protect, updateCoverLetter);
-router.put('/update/dream-job', protect, updateDreamJob);
-router.put('/update/job-types', protect, updatePreferredJobTypes);
-router.put('/update/work-environment', protect, updateWorkEnvPreferences);
-router.put('/update/locations', protect, updatePreferredLocations);
-router.put('/update/video-intro', protect, updateVideoIntro);
-router.put('/update/mobility', protect, updateMobilityPreferences);
-router.put('/update/interview-mode', protect, updateInterviewMode);
-router.put('/update/branding-statement', protect, updateBrandingStatement);
-router.put('/update/hobbies', protect, updateHobbies);
-router.put('/update/emergency-contact', protect, updateEmergencyContact);
-router.put('/update/social-links', protect, updateSocialLinks);
+// Generic Update Route
+router.patch('/user/updateUserProfile', protect, updateUserProfile);
 
-// Individual field get routes
-router.get('/name', protect, getName);
-router.get('/gender', protect, getGender);
-router.get('/pronouns', protect, getPronouns);
-router.get('/nationality', protect, getNationality);
-router.get('/resident-country', protect, getResidentCountry);
-router.get('/languages', protect, getKnownLanguages);
-router.get('/timezone', protect, getTimeZone);
-router.get('/qualification', protect, getQualification);
-router.get('/achievements', protect, getAchievements);
-router.get('/skills', protect, getSkills);
-router.get('/resume', protect, getResume);
-router.get('/cover-letter', protect, getCoverLetter);
-router.get('/dream-job', protect, getDreamJob);
-router.get('/job-types', protect, getPreferredJobTypes);
-router.get('/work-environment', protect, getWorkEnvPreferences);
-router.get('/locations', protect, getPreferredLocations);
-router.get('/video-intro', protect, getVideoIntro);
-router.get('/mobility', protect, getMobilityPreferences);
-router.get('/interview-mode', protect, getInterviewMode);
-router.get('/branding-statement', protect, getBrandingStatement);
-router.get('/hobbies', protect, getHobbies);
-router.get('/emergency-contact', protect, getEmergencyContact);
-router.get('/social-links', protect, getSocialLinks);
 
 export default router; 
