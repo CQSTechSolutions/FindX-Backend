fd2a7fb9a90d95aa37cbc4259d838c9f92fc02cf
Soumay-sanpui <somusanpui9@gmail.com>
2025-05-03 21:30:43 +0530

    ~ Login

--- controllers/employer.controller.js
+++ controllers/employer.controller.js
@@ -13,14 +13,21 @@ export const createAccount = async (req, res) => {
 export const login = async (req, res) => {
     const loginData = req.body;
     try {
-        const employer = await Employer.findOne({ companyEmployerId: loginData.companyEmployerId });
+        const employer = await Employer.findOne({ 
+            companyEmployerId: loginData.companyEmployerId,
+            EmployerEmail: loginData.EmployerEmail 
+        }).select('+password');
+
         if (!employer) {
             return res.status(404).json({ message: "Employer not found" });
         }
-        const isMatch  = Employer.comparePassword(loginData.password);
+
+        const isMatch = await bcrypt.compare(loginData.password, employer.password);
         if (!isMatch) {
-            return res.status(401).json({ message: "Invalid password" });
+            return res.status(401).json({ message: "Invalid Credentials" });
         }
+
+        employer.password = undefined;
         res.status(200).json(employer);
     } catch (error) {
         res.status(500).json({ message: error.message });
